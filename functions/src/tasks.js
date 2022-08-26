eb4eccd7032a02a669a99e9eb6e49a0ea251f7a3
james etchberger <etchbergerjames@gmail.com>
2022-08-26 10:50:16 -0400

    added get and post tasks

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,19 +1,42 @@
-export function getTasks(req, res) {
-    res.send('TASKS')
-} 
+import dbConnect from './dbconnect.js'
 
-export function createTask(req, res) {
-    const newTask = req.body
-    res.status(201).send('Task Added')
+export async function getTasks(req, res) {
+  const db = dbConnect()
+  const collection = await db
+    .collection('/tasks')
+    .get()
+    .catch((err) => res.status(500).send(err))
+  const tasks = collection.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id }
+    // let task = doc.data()
+    // task.id = doc.id
+    // return task
+  })
+  res.send(tasks)
+}
+
+export async function createTask(req, res) {
+  const newTask = req.body
+  if (!newTask || !newTask.task) {
+    res.status(400).send({ success: false, message: 'Invalid request' })
+    return
+  }
+  const db = dbConnect()
+  await db
+    .collection('tasks')
+    .add(newTask)
+    .catch((err) => res.status(500).send(err))
+  res.status(201)
+  getTasks(req, res) //send back the full list of tasks
 }
 
 export function updateTask(req, res) {
-    const taskUpdate = req.body
-    const { taskId } = req.params
-    res.status(202).send('Task Updated')
+  const taskUpdate = req.body
+  const { taskId } = req.params
+  res.status(202).send('Task Updated')
 }
 
 export function deleteTask(req, res) {
-    const { taskId } = req.params
-    res.status(203).send('Task Deleted')
+  const { taskId } = req.params
+  res.status(203).send('Task Deleted')
 }
